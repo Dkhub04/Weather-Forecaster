31181004716ceea7089ec254a05c54882400c7b1
karthik <donekarthikcoding.2004@gmail.com>
2024-07-28 14:43:38 +0530

    Final Little Changes Needed for Light/Dark Theme Code

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import Search from './components/search/search';
 import './App.css';
 import Weather from './components/weather/weather';
 import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import Forecast from './components/Forecast/forecast';
+// import { LIGHT_URL,DARK_URL } from './api';
+
 function App() {
+
+  // const [ThemeIMG,SetThemeIMG] = useState(LIGHT_URL);
+  //   const [Theme,SetTheme] = useState("light");
+
+  //   useEffect(()=>{
+  //       if(Theme === "dark"){
+  //           document.documentElement.classList.add("dark");
+  //       }else{
+  //           document.documentElement.classList.remove("dark");
+  //       }
+  //   },[Theme]);
+
+  //   const handleThemeSwith = ()=>{
+  //       SetTheme(Theme === "dark" ? "light" : "dark");
+  //       if(Theme === 'dark'){
+  //           SetThemeIMG(LIGHT_URL);
+  //       }
+  //       else{
+  //           SetThemeIMG(DARK_URL);
+
+  //       }
+  //   }
 
   const [currentweather, setCurrentweather] = useState(null);
   const [forecast, setForecast] = useState(null);
@@ -29,18 +53,22 @@ function App() {
       .catch(console.log);
   };
 
- 
-
-
   return (
-    <div className="font-bold max-w-5xl my-5 mx-auto  ">
-      <Search onsearchchange={handleonsearchchange} />
 
+  <div >
+      <div className="font-bold max-w-5xl my-5 mx-auto  ">
+      <Search onsearchchange={handleonsearchchange} />
+      
       {currentweather && <Weather data={currentweather} />}
 
       {forecast && <Forecast data= {forecast}/>}
-
     </div>
+    {/* <div className='flex'>
+        <div className="" onClick={()=>handleThemeSwith()}>
+              <img alt='button' className="w-[50px] h-[25px]" src={ThemeIMG}></img>
+        </div>
+    </div> */}
+</div>
   );
 }
 
--- src/api.js
+++ src/api.js
@@ -12,3 +12,9 @@ export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
 
 export const WEATHER_API_KEY = '3e4795e0e9d48ac2bdf10713c3d60560';
+
+
+
+export const DARK_URL = "https://homeworkify.st/wp-content/themes/Divi-child/images/dark-mode-toggle.svg"
+
+export const LIGHT_URL = "https://homeworkify.st/wp-content/themes/Divi-child/images/light-mode-toggle.png"
